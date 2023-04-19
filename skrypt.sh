7ada2c4e6a70cdef89744493f7680f4f8216588a
Krzysztof <kotinerus@gmail.com>
2023-04-19 21:03:34 +0200

    Final version

--- skrypt.sh
+++ skrypt.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 function menu(){
 	wartosc=$1
+	ile=$2
 	case "$wartosc" in
 	"--date"|"-d") 
 				today=$(date +'%d/%m/%Y')
@@ -19,8 +20,34 @@ function menu(){
 		echo ""
 		echo "--date lub -d - Wypisuje dzisiejszą datę"
 		echo "--logs n lub -l n - Tworzy n kolejnych plików"
-		echo "--help - lub -h Wyświetla wszystkie kmendy"
-		echo ""
-	esac
+		echo "--help  lub -h - Wyświetla wszystkie kmendy"
+		echo "--error n lub -e n - Tworzy n kolejnych errorów"
+		echo "";;
+	"--init"|"-i")
+		git clone https://github.com/kotinerus/narzedziaPracyWIT2
+		nowa_sciezka = $(pwd)
+		echo "export PATH=\$PATH:$nowa_sciezka" >> ~/.bashrc;;
+	"--error"|"-e")
+		if [ $# -gt 1 ]; then
+			mkdir error${ile}
+			for i in $(seq 1 $ile)
+				do	
+				 	echo "Utworzony przez: skrypt.sh --error ${ile}">error${ile}/error${i}.txt
+ 	                                echo "error${i}.txt">>error${ile}/error${i}.txt
+                	                echo  ${i}>>error${ile}/error${i}.txt
+        	                        today=$(date +'%d/%m/%Y');echo "Dziś jest: ${today}">>error${ile}/error${i}.txt
+				done
+		else
+			mkdir error${ile}
+			 for i in $(seq 1 100)
+                                do
+ 					echo "Utworzony przez: skrypt.sh --error ${ile}">error${ile}/error${i}.txt
+                                        echo "error${i}.txt">>error${ile}/error${i}.txt
+                                        echo  ${i}>>error${ile}/error${i}.txt
+                                        today=$(date +'%d/%m/%Y');echo "Dziś jest: ${today}">>error${ile}/error${i}.txt
+
+                                done
+		fi
+esac
 }
-menu "$1" "$2"
+menu "$@" 
